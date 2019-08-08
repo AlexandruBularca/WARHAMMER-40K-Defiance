0a1603fbfcf29c0b6677d0e02582003741325039
PavelBogdan <46486442+pavelbogdan71@users.noreply.github.com>
2019-08-08 13:59:27 +0300

    Update MapView.vue

--- src/components/gameScreens/viewTypes/MapView.vue
+++ src/components/gameScreens/viewTypes/MapView.vue
@@ -1,124 +1,127 @@
 <template>
-    <div class="mapHolder">
-        <div class="currentUserLocation" v-bind:style="locateTheUser"/>
-        <div class="buttonsHolder">
-            <button class="btnAttack" v-on:click="showCombatView()">Search</button>
-        </div>      
+  <div class="mapHolder">
+    <div class="locations">
+      <div class="currentUserLocation" v-for="item in mapLocationsJson">{{item.location}}</div>
     </div>
+    <div class="buttonsHolder">
+      <button class="btnAttack" v-on:click="showCombatView()">Search</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import mapLocationsJson from "./../../../assets/json/map_locations.json";
 export default {
-    name: 'Map', 
-    methods: {
-        showCombatView() {
-            this.$store.commit('combatMode')
-        },
-    },
-    data() {
-        return {
-            userPosX: this.$store.state.Map.location.x,
-            userPosY: this.$store.state.Map.location.y,
-        }
-    },
-    computed: {
-        locateTheUser: function () {
-            return {
-                top: this.userPosX + '%',
-                left: this.userPosY + '%',
-            }
-        }
+  name: "Map",
+  methods: {
+    showCombatView() {
+      this.$store.commit("combatMode");
     }
-}
+  },
+  data() {
+    return {
+      userPosX: mapLocationsJson.location_1.location.x,
+      userPosY: mapLocationsJson.location_1.location.y
+    };
+  },
+  computed: {
+    locateTheUser: function() {
+      return {
+        top: this.userPosX + "%",
+        left: this.userPosY + "%"
+      };
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 .mapHolder {
-    background-color: rgb(18, 233, 108);
-    background-image: url("./../../../assets/img/map.jpg");
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    height: 97vh;
-    width: 72.5vw;
-    border-color: rgb(184, 184, 184);
-    border-width: 4px;
-    border-style: solid;
-    margin-left: 26vw;
+  background-color: rgb(18, 233, 108);
+  background-image: url("./../../../assets/img/map.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 97vh;
+  width: 72.5vw;
+  border-color: rgb(184, 184, 184);
+  border-width: 4px;
+  border-style: solid;
+  margin-left: 26vw;
 }
-
+.locations {
+  position: relative;
+}
 .currentUserLocation {
-    height: 20px;
-    width: 20px;
-    background-color: #e910108f;
-    border-radius:50%;
-    box-shadow: 0 0 0 5px #fd843e85;
-    position:relative;
+  height: 20px;
+  width: 20px;
+  background-color: #e910108f;
+  border-radius: 50%;
+  box-shadow: 0 0 0 5px #fd843e85;
+  position: relative;
 }
 
 .buttonsHolder {
-    height: 10%;
-    width: 80%;
-    position: relative;
-    top: 87%;
-    left: 50%;
-    transform: translate(-50%,0%);
-}   
+  height: 10%;
+  width: 80%;
+  position: relative;
+  top: 87%;
+  left: 50%;
+  transform: translate(-50%, 0%);
+}
 
 .btnSearch {
-    width: 160px;
-    height: 45px;
-    font-family: 'Roboto', sans-serif;
-    font-size: 14px;
-    text-transform: uppercase;
-    letter-spacing: 2.5px;
-    font-weight: 500;
-    color: #000;
-    background-color: #fff;
-    border: none;
-    border-radius: 45px;
-    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-    transition: all 0.3s ease 0s;
-    cursor: pointer;
-    outline: none;
-    position: relative;
-    left: 50%;
-    transform: translate(-50%,0%);
+  width: 160px;
+  height: 45px;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 500;
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 0%);
 }
 
 .btnSearch:hover {
-    background-color: rgb(0, 0, 0);
-    box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.4);
-    color: #fff;
-    transform: translate(-50%, -20%);
-}   
+  background-color: rgb(0, 0, 0);
+  box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.4);
+  color: #fff;
+  transform: translate(-50%, -20%);
+}
 
 .btnAttack {
-    width: 160px;
-    height: 45px;
-    font-family: 'Roboto', sans-serif;
-    font-size: 14px;
-    text-transform: uppercase;
-    letter-spacing: 2.5px;
-    font-weight: 500;
-    color: #000;
-    background-color: #fff;
-    border: none;
-    border-radius: 45px;
-    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-    transition: all 0.3s ease 0s;
-    cursor: pointer;
-    outline: none;
-    position: relative;
-    left: 50%;
-    transform: translate(-50%,0%);
+  width: 160px;
+  height: 45px;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 500;
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 0%);
 }
 
 .btnAttack:hover {
-    background-color: rgb(0, 0, 0);
-    box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.4);
-    color: #fff;
-    transform: translate(-50%, -20%);
+  background-color: rgb(0, 0, 0);
+  box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.4);
+  color: #fff;
+  transform: translate(-50%, -20%);
 }
-
 </style>
