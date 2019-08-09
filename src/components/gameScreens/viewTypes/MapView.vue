e04eefd211d3cd741e936cab41f73f8fd20af0c7
Teodor G <grigor.teodor@gmail.com>
2019-08-09 16:47:14 +0300

    Update MapView.vue

--- src/components/gameScreens/viewTypes/MapView.vue
+++ src/components/gameScreens/viewTypes/MapView.vue
@@ -173,13 +173,42 @@ export default {
     outline: none;
     position: relative;
     left: 50%;
-    transform: translate(-50%, 0%);
+    transform: translate(-50%, 10%);
   }
   .btnAttack:hover {
     background-color: rgb(0, 0, 0);
     box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.4);
     color: #fff;
     transform: translate(-50%, -10%);
+  }
+}
+
+@media (max-width: 700px) {
+  .btnAttack {
+    width: 90px;
+    height: 24px;
+    font-family: "Roboto", sans-serif;
+    font-size: 8px;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-weight: 500;
+    color: #000;
+    background-color: #fff;
+    border: none;
+    border-radius: 45px;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+    outline: none;
+    position: relative;
+    left: 50%;
+    transform: translate(-50%, 10%);
+  }
+  .btnAttack:hover {
+    background-color: rgb(0, 0, 0);
+    box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.4);
+    color: #fff;
+    transform: translate(-50%, -5%);
   }
 }
 
