d050fdb754e714fd8a736105e9d05ec645509f22
Teodor G <grigor.teodor@gmail.com>
2019-08-09 16:21:40 +0300

    Update InventoryView.vue

--- src/components/gameScreens/viewTypes/InventoryView.vue
+++ src/components/gameScreens/viewTypes/InventoryView.vue
@@ -352,7 +352,12 @@ export default {
   border-style: solid;
   margin-left: 26vw;
   box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.37);
-  
+}
+
+@media (max-width: 1000px) {
+  .inventoryHolder {
+    border-width: 2px;
+  }
 }
 
 .inventory {
